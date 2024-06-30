602cb47c522825aaaa567428904d8142cb7f1da4
camilo-evidencias <archivos.desarrollo.proyectos@gmail.com>
2024-06-30 15:34:05 -0500

    first commit

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-699.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-k8gv6fkj' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-c69aee6e' );
+});
+ });
---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-720.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-mykdzf9o' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-u6wkcvwh' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-afjk9ifa' );
+});
+ });
