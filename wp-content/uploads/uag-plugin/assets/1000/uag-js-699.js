54cc2eabc5a364a44b9baff5a07601183a5afc2a
camilo-evidencias <archivos.desarrollo.proyectos@gmail.com>
2024-07-01 14:18:39 -0500

    WordpressHTML

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-699.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-699.js
@@ -4,4 +4,9 @@ document.addEventListener("DOMContentLoaded", function(){ window.addEventListene
 window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-c69aee6e' );
 });
+window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-652739c1 .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-652739c1 .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-652739c1 .swiper-button-next","prevEl":".uagb-block-652739c1 .swiper-button-prev"}}	);
+});
+
  });
