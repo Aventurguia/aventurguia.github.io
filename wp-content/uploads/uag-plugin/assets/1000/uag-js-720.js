d1c25b804c87e55d6b0b6442e1ca7e9a9c853ea6
camilo-evidencias <archivos.desarrollo.proyectos@gmail.com>
2024-07-18 02:34:55 -0500

    wordpress rutas

--- wp-content/uploads/uag-plugin/assets/1000/uag-js-720.js
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-720.js
@@ -7,4 +7,22 @@ window.addEventListener( 'load', function() {
 window.addEventListener( 'load', function() {
 	UAGBButtonChild.init( '.uagb-block-afjk9ifa' );
 });
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-101af3e1' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-9bd7876c' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-73c7795d' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-173b3dd3' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-8032a183' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-e91b7318' );
+});
  });
